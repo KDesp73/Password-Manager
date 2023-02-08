b857afc5ca91510863660f8931b2c4daaa4e7e52
KDESP-LAPTOP\Konstantinos <kdesp2003@gmail.com>
2023-02-08 19:18:22 +0200

    Commit all

--- Password-Manager/src/main/java/Main/Main.java
+++ Password-Manager/src/main/java/Main/Main.java
@@ -1,17 +1,12 @@
 package Main;
 
-import Commands.Functions;
 import Commands.Commands;
-import Database.Database;
-import Database.SQLMethods;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
         public static void main(String[] args) throws SQLException {
                 PasswordCollection.refresh();
                 Commands.matchCommands(Commands.promt());
-
+                
         }
 }
